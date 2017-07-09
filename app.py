e67b41c42152e8f1ae673ee5240144902f82b4dd
prashasy <prashasyashok@gmail.com>
2017-07-10 00:53:42 +0530

    Important Additions.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sqlite3 as sql
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
-from forms import LoginForm
+from forms import *
 
 
 appy=Flask(__name__)
@@ -10,8 +10,52 @@ appy.secret_key="Hi"
 
 def insertData(form):
     con=sql.connect("database.db")
-    con.row_factory=sql.Row
     cur=con.cursor()
+    a=form.enrollment.data
+    un=form.username.data
+    co=cur.execute("select count(*) from login_data where enrollment=?",(a,))
+    co2=cur.execute("select count(*) from login_data where username=?",(un,))
+    if ((co.fetchall())[0][0]!=0 or (co2.fetchall())[0][0]!=0) :
+	    return "ERROR"
+	    return render_template('register.html',form=form)
+    else:
+    	b=form.username.data
+    	c=form.password.data
+    	d=form.name.data
+    	e=form.phone.data
+    	cur.execute("insert into login_data(enrollment,username,password)values(?,?,?)",(int(a),str(b),str(c)))
+    	print("Data inserted into login_data successfully!!")
+    	cur.execute("insert into user_data(enrollment,name,phone)values(?,?,?) ",(int(a),str(d),int(e)) )
+    	print("Data inserted into user_data successfully!!")
+    	con.commit()
+    	return render_template('index.html')
+
+
+
+
+def do_login(form):
+	user=form.uname.data
+	pa=form.pw.data
+	con=sql.connect('database.db')
+	cur=con.cursor();
+	count=cur.execute('select count(*) as count from login_data where username=? and password=? ',(user,pa,)  )
+	n=(count.fetchall())[0][0]
+	if (n==1):
+		session['logged_in']=True
+		r=((cur.execute('select enrollment from login_data where username=? and password=? ',(user,pa,)  )).fetchall())[0][0]
+		result=(con.execute("select name from user_data where enrollment=?",(r,)).fetchall())[0]
+		session['login_user']=result[0]
+		return redirect('/')
+	else:
+		return render_template('login.html',form=form,msg="Invalid Credentials")
+
+def do_logout():
+	form=LoginForm()
+	session['logged_in']=False
+	session['login_user']=""
+	return render_template('login.html',form=form,msg="You have logged out")
+
+
     
 	
 
@@ -20,14 +64,11 @@ def insertData(form):
 @appy.route('/',methods=['GET','POST'])
 def home():
 	if ((session.get('logged_in'))):
-		return "Hello!!"
+		return render_template('index.html',user=session['login_user'])
 
 
 	else:
-		form = LoginForm()
-		return render_template('login.html', 
-		                       title='Sign In',
-		                       form=form)
+		return render_template('index.html',title="Home")
 
 
 
@@ -40,15 +81,37 @@ def profile(username):
 
 @appy.route('/register',methods=['GET','POST'])
 def register():
+	form=SignUpForm(request.form)
+	if(request.method=='POST'):
+		if(form.validate_on_submit()==False):
+			return render_template('register.html',form=form)
+		else:
+			 return insertData(form)
+	elif(request.method=="GET"):
+		return render_template('register.html',form=form)
+
+
+@appy.route('/login',methods=['GET','POST'])
+def login():
+	if(session.get('logged_in')):
+		return redirect('/')
 	form=LoginForm(request.form)
 	if(request.method=='POST'):
 		if(form.validate_on_submit()==False):
-			print (form.errors)
-			return render_template('register.html',form=form)
+			return render_template('login.html',form=form)
 		else:
-			return insertData(form)
-	elif(request.method=="GET"):
-		return render_template('register.html',form=form)
+			return do_login(form)
+	elif(request.method=='GET'):
+		return render_template('login.html',form=form)
+
+@appy.route('/logout')
+def logout():
+	if(session.get('logged_in')):
+		return do_logout() 
+	else:
+		return redirect('/')
+
+
 if(__name__=='__main__'):
 	appy.secret_key="Hi"
 	appy.run(debug=True)
--- forms.py
+++ forms.py
@@ -3,10 +3,18 @@ from wtforms import *
 from wtforms.fields.html5 import *
 from wtforms.widgets.html5 import *
 #from flask.ext.wtf.html5 import NumberInput,TelField
-class LoginForm(_Form):
+class SignUpForm(_Form):
 	name=TextField("Name",[validators.Required("Plese Enter Your Name!!")])
 	enrollment=IntegerField("Enrollment Number",[validators.Required("Please enter your enrollment number!!")],widget=NumberInput())
 	phone=TelField("Your Contact Number please!",[validators.Required("Please enter your phone number")])
 	username=TextField("Choose Username",[validators.Required("Plese Enter Your Username!!")])
 	password=PasswordField("Chosse a password",[validators.Required("Plese Enter Your Password!!")])
 	submit=SubmitField("Submit")
+
+
+class LoginForm(_Form):
+	uname=TextField("User-Name",[validators.Required("Plese Enter Your User-Name!!")])
+	pw=PasswordField("Your Password",[validators.Required("Plese Enter Your Password!!")])
+	submit=SubmitField("Login")
+
+
